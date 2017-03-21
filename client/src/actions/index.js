4404c1c65d4172afef1371b9de3e4b25f909a981
bill bittner <bittner.w@gmail.com>
2017-03-21 10:47:31 -0700

    adding a new fan refreshes the events array and activeEvent obj in app state

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export const FETCH_VENUE = "FETCH_VENUE";
 export const FETCH_EVENTS = "FETCH_EVENTS";
 export const EVENT_SELECTED = "EVENT_SELECTED";
+export const REFRESH_ACTIVE_EVENT = "REFRESH_ACTIVE_EVENT";
 
 // this method gets all data for a venue based on user Id (must be the owner)
 export function fetchVenue(userId, authToken){
@@ -25,7 +26,7 @@ export function fetchVenue(userId, authToken){
 // this method gets all event data for a venue based on venue id
 export function fetchEvents(venueId, authToken){
     
-    const url = `/api/event/${venueId}`;
+    const url = `/api/event/all/${venueId}`;
     const config = {
         headers: {"Authorization": `bearer ${authToken}`}
     };
@@ -35,6 +36,24 @@ export function fetchEvents(venueId, authToken){
 
     return {
         type: FETCH_EVENTS,
+        payload: request
+    }
+
+}
+
+// this method gets all data for an event based on an event id
+export function refreshActiveEvent(eventId, authToken){
+    
+    const url = `/api/event/one/${eventId}`;
+    const config = {
+        headers: {"Authorization": `bearer ${authToken}`}
+    };
+    const request = axios.get(url, config);
+
+    console.log("refreshActiveEvent request:", url, config)
+
+    return {
+        type: REFRESH_ACTIVE_EVENT,
         payload: request
     }
 
