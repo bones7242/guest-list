3fc7dd5d12686e7a9642981752fb4520e3eb4b66
bill bittner <bittner.w@gmail.com>
2017-03-24 10:56:50 -0700

    added decrement to counter function

--- client/src/components/subComponents/Guest.jsx
+++ client/src/components/subComponents/Guest.jsx
@@ -13,39 +13,75 @@ class Guest extends Component {
 		console.log("asdfasdfasdfasdf", props);
 		// get parent props 
 		super(props);
+		// set initial state
+		this.state = {
+			active: null
+		}
 		// bind this to functions in this component 
 		this.checkInGuest = this.checkInGuest.bind(this);
 	}
+	// helper method for toggling button state
+	toggle(position){
+		if (this.state.active === position){
+			this.setState({active: null})
+		} else {
+			this.setState({active: position})
+		}
+	}
+	myColor(position){
+		if (this.state.active === position) {
+			return "grey";
+		} 
+		//otherwise...
+		return "#4527a0";
+	}
+	myBtnText(position){
+		if (this.state.active === position) {
+			return "UNDO";
+		} 
+		//otherwise...
+		return "ENTERED";
+	}
+
 	// helper method to update the event in the database.  if successfull, it tells redux to fetch the newest events list and activeEvent.  The user stays on this event page.
     checkInGuest(event){
 		// Prevent default action.  in this case, action is the form submission event.
         event.preventDefault();
+		
 		// create the update object 
 		const checkInObject = {
 			eventId: this.props.guest.eventId,
 			plusOne: this.props.guest.plusOne,
 		}
 		console.log("checkInGuest called:", checkInObject);
+
+		// decide wheter to increment or decriment guests 
+		let queryUrl = "";
+		if (this.state.active === null){
+			queryUrl = "/api/event/counter/increment";
+		} else {
+			queryUrl = "/api/event/counter/decrement";
+		}
+
         // add the new event to the mongo database 
         const xhr = new XMLHttpRequest();
-        xhr.open("PUT", "/api/event/counter/increment");
+        xhr.open("PUT", queryUrl);
         xhr.setRequestHeader("Authorization", `bearer ${Auth.getToken()}`);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.responseType = "json";
         xhr.addEventListener("load", () => {
             if (xhr.status === 200) {
 				console.log("success! response:", xhr.response)
-				alert("Guest was successfully checked in.");
 				// update the "active event"" in the application state
 				this.props.refreshActiveEvent(this.props.activeEvent._id, Auth.getToken());
 				// update the specific event in the "events"array in the applicaiton state  
 				this.props.fetchEvents(this.props.activeEvent.venue, Auth.getToken());
 				// diable this button
-
+				this.toggle(0);
 
             } else {
-				console.log("there was an error in creating the event. error message:", xhr.response.message)
-				alert("Event could not be added.  Check the console logs");
+				console.log("there was an error in updating the event. error message:", xhr.response.message)
+				alert("Event could not be updated.  Check the console logs.");
 			};
         });
         xhr.send(JSON.stringify(checkInObject)); //note: stringify an issue for numbers?
@@ -73,7 +109,7 @@ class Guest extends Component {
 					{this.props.guest.supportThreeList && <p>{this.props.activeEvent.supportThree}</p>}
 				</td>
 				<td>
-					<button className="waves-effect waves-light btn deep-purple darken-3 hoverable" onClick={this.checkInGuest}>ENTERED</button>
+					<button className="waves-effect waves-light btn hoverable" style={{backgroundColor: this.myColor(0)}} onClick={this.checkInGuest}>{this.myBtnText(0)}</button>
 				</td>
 				<td>
 					
