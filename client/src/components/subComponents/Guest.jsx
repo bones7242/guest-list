e4a986cfa79ed835a3bd5cec76258937d3e18272
bill bittner <bittner.w@gmail.com>
2017-03-25 11:39:43 -0700

    updated icons

--- client/src/components/subComponents/Guest.jsx
+++ client/src/components/subComponents/Guest.jsx
@@ -104,9 +104,9 @@ class Guest extends Component {
 				</td>
 				<td>
 					
-					<a className="btn-floating btn-small waves-effect waves-light blue-grey lighten-2 hoverable" style={{margin:"3px"}}> <i class="material-icons">delete</i></a>
+					<a className="btn-floating btn-small waves-effect waves-light blue-grey lighten-2 hoverable" style={{margin:"3px"}}> <i className="material-icons">delete</i></a>
 					
-					<a className="btn-floating btn-small waves-effect waves-light blue-grey lighten-2 hoverable" style={{margin:"3px"}}> <i class="material-icons">mode_edit</i></a>
+					<a className="btn-floating btn-small waves-effect waves-light blue-grey lighten-2 hoverable" style={{margin:"3px"}}> <i className="material-icons">mode_edit</i></a>
 				</td>
 			</tr>
 		);
