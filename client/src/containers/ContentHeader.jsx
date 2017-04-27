dd9fe13a4047a73808a89a03fd35ddf134d16078
louiskane <lkane02@yahoo.com>
2017-04-26 23:06:27 -0700

    adding data button to contentHeader

--- client/src/containers/ContentHeader.jsx
+++ client/src/containers/ContentHeader.jsx
@@ -110,6 +110,9 @@ class ContentHeader extends Component {
                 </div>
                 <div className="col s2 m2 l2 right-align">
                     <Link className="grey darken-2 btn-floating btn-small waves-effect waves-light hoverable" to="/dash/edit-event">
+                        <i className="material-icons">track_changes</i>
+                    </Link>
+                    <Link className="grey darken-2 btn-floating btn-small waves-effect waves-light hoverable" to="/dash/edit-event">
                         <i className="material-icons">mode_edit</i>
                     </Link>
                     <Link className="grey darken-2 btn-floating btn-small waves-effect waves-light hoverable" onClick={this.deleteEvent}>
